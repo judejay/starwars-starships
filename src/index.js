47082fff5bf82b581648a7b196aa58eb6b6898e9
judejay <judejay26@gmail.com>
2023-06-20 11:48:33 +0100

    remove unused reference

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { QueryClient , QueryClientProvider  } from '@tanstack/react-query';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const client = new QueryClient();
 root.render(
-    <QueryClientProvider client={client}>
        <App /> 
-    </QueryClientProvider>    
 );
 
